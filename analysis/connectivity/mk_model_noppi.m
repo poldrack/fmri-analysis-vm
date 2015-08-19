9177fae0a8a5b73682c45c60302b780ba7f2d8de
Russ Poldrack <poldrack@gmail.com>
2015-08-19 00:28:37 +0000

    fixed paths

--- analysis/connectivity/mk_model_noppi.m
+++ analysis/connectivity/mk_model_noppi.m
@@ -1,5 +1,5 @@
-load('DCM_truemodel.mat')
+load('/home/vagrant/fmri-analysis-vm/analysis/connectivity/dcmfiles/DCM_truemodel.mat')
 DCM.b=zeros(5,5);
 DCM = rmfield(DCM,'M'); % This line is needed if the template model is already estimated
 DCM = spm_dcm_estimate(DCM);
-save('DCM_noppi.mat','DCM');
+save('/home/vagrant/fmri-analysis-vm/analysis/connectivity/dcmfiles/DCM_noppi.mat','DCM');

a420e654e387d0292ef0b877db988faaa4535364
poldrack <poldrack@gmail.com>
2015-08-20 10:55:24 -0700

    added title arg

--- analysis/utils/graph_utils.py
+++ analysis/utils/graph_utils.py
@@ -1,4 +1,4 @@
-def show_graph_from_adjmtx(A,B,C):
+def show_graph_from_adjmtx(A,B,C,title=''):
 	import matplotlib.pyplot as plt
 	import networkx as nx
 	import numpy as np
@@ -6,14 +6,14 @@ def show_graph_from_adjmtx(A,B,C):
 	gr = nx.DiGraph()
 	nodes=range(1,A.shape[0])
 	gr.add_nodes_from(nodes)
-
+	gr.add_node('u')
 	rows, cols = np.where(A == 1)
 	edges_A = zip(cols.tolist(), rows.tolist())
 	gr.add_edges_from(edges_A)
 	rows, cols = np.where(B == 1)
 	edges_B = zip(cols.tolist(), rows.tolist())
 	gr.add_edges_from(edges_B)
-	mylabels={i:'%d'%i for i in gr.nodes()} # {(gr.nodes(),['%d'%i for i in gr.nodes()])
+	mylabels={i:'%d'%i for i in gr.nodes() if not i=='u'} # {(gr.nodes(),['%d'%i for i in gr.nodes()])
 	rows=np.where(C==1)[0]
 	edges_C=[]
 	for r in rows:
@@ -31,6 +31,7 @@ def show_graph_from_adjmtx(A,B,C):
 	if edges_B:
 		nx.draw_networkx_edges(gr,pos,edgelist=edges_B,edge_color='b')
 		print 'Blue: modulated'
+	plt.title(title)
 	plt.show()
 	return gr
 
@@ -41,7 +42,7 @@ def show_graph_from_pattern(pattern_file,nnodes=5):
 	pf=[i.strip().replace('"','') for i in open(pattern_file).readlines()]
 	if not pf[0].find('digraph')>-1:
 		raise RuntimeError('input is not a valid dot file')
-		
+
 	gr = nx.DiGraph()
 	nodes=[]
 	edges=[]
@@ -64,7 +65,7 @@ def show_graph_from_pattern(pattern_file,nnodes=5):
 			assert l_s[4].find('arrowhead')>-1
 			if l_s[4].find('none')>-1:
 				edges.append((n2,n1))
-				
+
 	nodes=range(0,nnodes) # include any nodes that had no connnections
 	mylabels={i:'%d'%i for i in nodes} # {(gr.nodes(),['%d'%i for i in gr.nodes()])
 	mylabels['u']='u'

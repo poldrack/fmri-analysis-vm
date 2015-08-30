0c6246c29c94a0e13fb55470e80ed8116875a98e
poldrack <poldrack@gmail.com>
2015-08-30 07:35:37 +0000

    initial add

---
+++ analysis/firstlevel/fix_json.py
@@ -0,0 +1,23 @@
+"""
+fix repetitoin time in json for downsampled data
+"""
+
+import shutil
+
+infile='/home/vagrant/data/ds031/sub00001/ses014/functional/sub00001_ses014_task002_run001_bold.json'
+
+lines=open(infile).readlines()
+newlines=[]
+for l in lines:
+	if l.find('RepetitionTime')>-1:
+		if l.find('1.16')>-1:
+			newlines.append(l.replace('1.16','2.32'))
+		print newlines[-1]
+	else:
+		newlines.append(l)
+shutil.move(infile,infile.replace('.json','_orig.json'))
+f=open(infile,'w')
+for l in newlines:
+	f.write(l)
+f.close()
+		

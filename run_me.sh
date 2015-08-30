9848ca8fa1537c1c3ff4e8ec723023af63a75a10
poldrack <poldrack@gmail.com>
2015-08-30 07:39:42 +0000

    added decimation and json fix

--- run_me.sh
+++ run_me.sh
@@ -2,3 +2,7 @@
 
 pip install seaborn
 pip install --upgrade https://github.com/nipy/nipype/archive/master.zip
+
+python analysis/firstlevel/fix_json.py
+python analysis/firstlevel/downsample_fmri.py
+

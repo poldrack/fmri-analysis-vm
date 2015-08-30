fe01ac3e913f9cbefb98eda34cdedc96e9d07205
poldrack <poldrack@gmail.com>
2015-08-30 07:43:54 +0000

    added h5py instal

--- run_me.sh
+++ run_me.sh
@@ -2,6 +2,7 @@
 
 pip install seaborn
 pip install --upgrade https://github.com/nipy/nipype/archive/master.zip
+conda install h5py
 
 python analysis/firstlevel/fix_json.py
 python analysis/firstlevel/downsample_fmri.py

7b6208f55fe69f6357bdc9c3f0c735911a69510e
Russ Poldrack <poldrack@gmail.com>
2016-11-21 19:58:45 +0000

    grab tarball

--- get_resting.sh
+++ get_resting.sh
@@ -5,12 +5,7 @@ then
 fi
 cd sub-01
 
-if [ ! -d ses-105 ] 
-then
-   mkdir ses-105
-   mkdir ses-105/func
-fi
-cd ses-105/func
-
-wget http://web.stanford.edu/group/poldracklab/myconnectome-data/raw_rsfmri/rsfmri_nifti/ses-105/func/sub-01_ses-105_task-rest_run-001_bold.json 
-wget http://web.stanford.edu/group/poldracklab/myconnectome-data/raw_rsfmri/rsfmri_nifti/ses-105/func/sub-01_ses-105_task-rest_run-001_bold.nii.gz 
+wget --cut-dirs=6  http://web.stanford.edu/group/poldracklab/myconnectome-data/raw_rsfmri/rsfmri_nifti/ses-105.tgz
+#wget --cut-dirs=6 --recursive  http://web.stanford.edu/group/poldracklab/myconnectome-data/raw_rsfmri/rsfmri_nifti/ses-105/fmap
+tar zxvf ses-105.tgz
+rm ses-105.tgz
